d223d4ec49145cf1fc19af998410d42dda4a9622
shungo0222 <kimura.shungo@gmail.com>
2023-04-10 08:59:26 +0900

    Fix the wallet connect function

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,38 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import getWeb3 from '../utils/web3'
-import Web3 from 'web3'
 
 const Navbar = () => {
-	const [web3, setWeb3] = useState<typeof Web3 | null>(null)
 	const [account, setAccount] = useState<string | null>(null)
 
-	useEffect(() => {
-		const init = async () => {
-			const web3 = await getWeb3()
-			setWeb3(web3)
-		}
-		init()
-	}, [])
-
 	const handleConnectWallet = async () => {
-		if (web3) {
-			try {
-				// Connect to Metamask
-				await web3.currentProvider.enable()
-				console.log('Connected to Metamask')
-
-				// Get accounts
-				const accounts = await web3.eth.getAccounts()
-				setAccount(accounts[0])
-				console.log('Connected to Metamask with account:', accounts[0])
-			} catch (error) {
-				console.log(error)
-			}
-		} else {
-			console.log('Web3 is not initialized')
-		}
+		try {
+      const web3 = await getWeb3()
+      const accounts = await web3.eth.getAccounts()
+      setAccount(accounts[0])
+      console.log('Connected to Metamask with account:', accounts[0])
+    } catch (error) {
+      console.error(error)
+    }
 	}
+
+	const handleDisconnectWallet = () => {
+    setAccount(null)
+  }
 
 	return (
 		<div className='fixed left-0 top-0 w-full bg-white z-10'>
@@ -40,7 +26,7 @@ const Navbar = () => {
 				<Link href='/'>
 					<h1 className='font-bold text-4xl'>TransPay</h1>
 				</Link>
-				<button onClick={handleConnectWallet} className='px-8 py-2 border font-bold bg-gray-200 rounded-lg'>{account ? account : 'Connect Wallet'}</button>
+				<button onClick={account ? handleDisconnectWallet : handleConnectWallet} className='px-8 py-2 border font-bold bg-gray-200 rounded-lg'>{account ? `Disconnect Wallet: ${account}` : 'Connect Wallet'}</button>
 			</div>
 		</div>
 	)
