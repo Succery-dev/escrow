1950538f62351da15bd6bc79c85dbd776925718e
shungo0222 <kimura.shungo@gmail.com>
2023-05-24 17:15:55 +0900

    Resolve conflicts: add modified code for HP for large devices

---
+++ components/index.tsx
@@ -0,0 +1,25 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import CustomButton from "./CustomButton";
+
+// Aesthetic Imports
+import { Glow } from "./aesthetics";
+
+// HP Imports
+import {
+  IntroSection,
+  Features,
+  Walkthrough,
+  CurrentSystemProblems,
+} from "./HP";
+
+export {
+  Navbar,
+  IntroSection,
+  Footer,
+  Features,
+  Walkthrough,
+  CurrentSystemProblems,
+  CustomButton,
+  Glow,
+};
