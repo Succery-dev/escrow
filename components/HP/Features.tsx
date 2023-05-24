1d54ee857d4ba9ae785e11e6d562c79d277ed937
shungo0222 <kimura.shungo@gmail.com>
2023-05-24 17:40:44 +0900

    Resolve conflicts: Fix the error: Module not found: Can't resolve 'fs'

--- components/HP/Features.tsx
+++ components/HP/Features.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Image from "next/image";
-import Tilt from "react-parallax-tilt";
 
 // External Package Component Imports
 import Tilt from "react-parallax-tilt";
@@ -12,7 +11,6 @@ import { features } from "../../constants";
 import { motion } from "framer-motion";
 import { fadeIn, textVariant } from "../../utils/motion";
 
-<<<<<<< HEAD
 // Interface Imports
 import { FeatureInterface } from "../../interfaces";
 
@@ -55,9 +53,6 @@ const FeaturesCard = ({
 };
 
 const Features = (): JSX.Element => {
-=======
-const Features = () => {
->>>>>>> cac6e74 (add responsive HP code with animation on scroll)
   return (
     <div id="features">
       <motion.h1
@@ -73,41 +68,11 @@ const Features = () => {
         {features.map((feature, index) => {
           return (
             // Card
-<<<<<<< HEAD
             <FeaturesCard
               feature={feature}
               index={index}
               key={`feature-${index}`}
             />
-=======
-            <Tilt key={index}>
-              <motion.div
-                variants={fadeIn("right", 1.25, index)}
-                initial="hidden"
-                whileInView="visible"
-                viewport={{ once: true, amount: 0.25 }}
-                className="blue-transparent-green-gradient-vertical xl:w-[450px] lg:w-[350px] lg:p-[3px] p-[2px] sm:min-h-0 min-h-[300px] rounded-2xl grid place-items-center"
-              >
-                <div className="bg-bg_primary w-full h-full flex flex-col xl:px-8 lg:px-6 sm:px-3 px-4 xl:py-12 lg:py-10 py-8 rounded-2xl">
-                  {/* Card Heading */}
-                  <div className="flex flex-row items-center lg:gap-8 sm:gap-1 gap-8">
-                    <Image
-                      src={feature.image}
-                      alt={feature.title}
-                      className="w-auto xl:h-[70px] lg:h-[50px] sm:h-[40px] h-[60px]"
-                    />
-                    <h2 className=" xl:text-4xl lg:text-3xl sm:text-xl text-2xl font-extrabold">
-                      {feature.title}
-                    </h2>
-                  </div>
-                  {/* Card Description */}
-                  <p className=" mt-8 px-6 font-normal xl:text-2xl lg:text-xl sm:text-sm text-xl">
-                    {feature.description}
-                  </p>
-                </div>
-              </motion.div>
-            </Tilt>
->>>>>>> cac6e74 (add responsive HP code with animation on scroll)
           );
         })}
       </div>
